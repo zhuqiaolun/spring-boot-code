71799f68bd1a0b68aa8376d04fe80edbb808664e
zhuqiaolun <zhuqlchina@163.com>
2020-05-22 17:57:30 +0800

    1.新增测试内容 2.删除非代码文件

--- src/test/java/com/demon/springbootcode/SpringBootCodeApplicationTests.java
+++ src/test/java/com/demon/springbootcode/SpringBootCodeApplicationTests.java
@@ -3,11 +3,14 @@ package com.demon.springbootcode;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+
 @SpringBootTest
 class SpringBootCodeApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(new Date());
     }
 
 }
