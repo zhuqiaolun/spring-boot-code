bb37e934c74e416fbca8f8f7bc0f0f737a2b692f
zhuqiaolun <zhuqlchina@163.com>
2020-05-22 17:52:01 +0800

    IDEA整合GitHub测试

---
+++ src/main/java/com/demon/springbootcode/SpringBootCodeApplication.java
@@ -0,0 +1,13 @@
+package com.demon.springbootcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demon/springbootcode/SpringBootCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demon.springbootcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
